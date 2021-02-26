a977edc21951a0a4b00758a6f71531373329964b
Fanrr <2514509399@qq.com>
2021-02-26 14:03:49 +0800

    完成点击切换开关

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    <button>
-      <span></span>
+    <button @click="toggle" :class="{checked}">
+      <span>
+        <span class="off">关</span>
+        <span class="on">开</span>
+      </span>
     </button>
-
+    {{ checked }}
   </div>
 </template>
 
 <script lang="ts">
+import {ref} from 'vue';
+
 export default {
-  name: 'Switch'
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return {checked, toggle};
+  }
 };
 </script>
 
@@ -23,7 +34,7 @@ button {
   background: gray;
   border-radius: $h/2;
   position: relative;
-
+  font-size: 8px;
   > span {
     position: absolute;
     top: 2px;
@@ -32,13 +43,24 @@ button {
     width: $h2;
     background: white;
     border-radius: $h2/2;
+    > .on{
+      display: none;
+    }
   }
-
-  span:hover {
-    left: calc(100% - #{$h2} - 2px); // 移到最外面再向内移一个span的宽度加两像素
-  }
-
 }
 
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px); // 移到最外面再向内移一个span的宽度加两像素
+  > .on{
+    display: block;
+  }
+  > .off{
+    display: none;
+  }
+}
+
+button.checked {
+  background: blue;
+}
 
 </style>
