60572fb5905e7a7410141cc8ba189e6d750a077f
Fanrr <2514509399@qq.com>
2021-03-01 14:03:56 +0800

    dialog的toggle可以用了

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,24 @@
 <template>
-<div>我是DialogDemo</div>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="visible"></Dialog>
 </template>
 
 <script>
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {ref} from 'vue'
+
 export default {
-  name: 'DialogDemo'
+  setup() {
+    const visible = ref(false)
+    const toggle = () => {
+      visible.value = !visible.value
+    }
+    return {visible, toggle}
+  },
+  components: {Button, Dialog}
 }
 </script>
 
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,13 +1,110 @@
 <template>
-
+  <template v-if="visible">
+    <div class=" wheel-dialog-overlay"></div>
+    <div class=" wheel-dialog-wrapper">
+      <div class=" wheel-dialog">
+        <header>标题 <span class=" wheel-dialog-close"></span></header>
+        <main>
+          <p>第一行字</p>
+          <p>大二行字</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
+    </div>
+  </template>
 </template>
 
-<script>
+<script lang="ts">
+import Button from './Button.vue';
+
 export default {
-  name: 'Dialog'
-}
+  components: {Button},
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+};
 </script>
 
-<style scoped>
+<style lang="scss">
+$radius: 4px;
+$border-color: #9d9d9d;
+
+.wheel-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade-out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+
+  &-overlay {
+    border: 1px solid red;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade-out(black, 0.5);
+    z-index: 10;
+  }
+
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+
+  > header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 20px;
+  }
+
+  > main {
+    padding: 12px 16px;
+  }
+
+  > footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+
+    &::before, &::after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
 
 </style>
