00f33bcc3b1c045d86716b48a7af6679f45088d7
Fanrr <2514509399@qq.com>
2021-03-03 15:37:36 +0800

    indicator的宽度可以和选中的nav等宽了

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>Tabs 示例</div>
   <h1>示例1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航一">内容一</Tab>
     <Tab title="导航二">内容二</Tab>
   </Tabs>
@@ -10,13 +10,13 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue';
 import Tab from '../lib/Tab.vue';
+import {ref} from 'vue';
 
 export default {
-  name: 'TabsDemo',
-  components: {Tab, Tabs}
+  components: {Tab, Tabs},
+  setup() {
+    const x = ref('导航一');
+    return {x};
+  }
 };
 </script>
-
-<style scoped>
-
-</style>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,42 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+  <div class="wheel-tabs">
+    <div class="wheel-tabs-nav">
+      <div class="wheel-tabs-nav-item"
+           v-for="(t,index) in titles" :key="index"
+           :class="{selected:t===selected}" @click="select(t)"
+           :ref="el=>{ if (el) navItems[index]=el}"
+      >{{ t }}
+      </div>
+      <div class="wheel-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="wheel-tabs-content">
+      <component class="wheel-tabs-content-item"
+                 v-for="(c,index) in defaults" :is="c" :key="index"
+                 :class="{selected:c.props.title === selected}"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 
 import Tab from './Tab.vue';
+import {ref, onMounted} from 'vue';
 
 export default {
-  name: 'Tabs',
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    onMounted(() => {
+      const divs = navItems.value; //获取nav的div
+      const result = divs.filter(div => div.classList.contains('selected'))[0]; //筛选出含有selected的元素
+      const {width} = result.getBoundingClientRect();
+      indicator.value.style.width = width + 'px'; //让indicator的宽度等于选中Nav的宽度
+    });
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -19,13 +44,66 @@ export default {
       }
     });
     const titles = defaults.map((tag) => {
-      return tag.props.title;
+      return tag.props.title; //拿到Tab.title内容
     });
-    return {defaults, titles};
+
+    const select = (title: String) => {
+      context.emit('update:selected', title);
+    };
+    return {defaults, titles, select, navItems, indicator};
   }
 };
 </script>
 
-<style>
+<style lang="scss">
+$green: #088300;
+$color: #333;
+$border-color: #d9d9d9;
+
+.wheel-tabs {
+
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $green;
+      }
+    }
+
+    &-indicator {
+      position: absolute;
+      width: 100px;
+      height: 3px;
+      background: $green;
+      bottom: -1px;
+      left: 0;
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
+
 
 </style>
