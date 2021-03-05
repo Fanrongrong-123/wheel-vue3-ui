db930a1811a2ce4057d27f840c89c215cc140ab1
Fanrr <2514509399@qq.com>
2021-03-06 00:23:16 +0800

    支持markdown

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="markdown-body">
-    <article>
-      <h1>介绍：</h1>
-      <p>Wheel UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
-      <p>这款组件库是我为了学习Vue3所写，全程亲手编写，尽量没有采用第三方库。</p>
-      <p>包括你现在看到的这个官网也几乎都是我自己写的，
-      <p>所以如果强烈不建议你将此 UI 库用于生产环境。</p>
-      <p>但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的，源代码放在了<a href="#">GitHub</a>上。</p>
-      <p>历史提交信息非常规范，可以按提交的顺序逐个查看，也可以直接查看每个组件的源代码和示例</p>
-      <p>运行方法见 README.md。</p>
-      <p>下一节：<a href="#/doc/install">安装</a></p>
-    </article>
-  </div>
+  <article class="markdown-body">
+    <h1>介绍：</h1>
+    <p>Wheel UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <p>这款组件库是我为了学习Vue3所写，全程亲手编写，尽量不采用第三方库</p>
+    <p>包括你现在看到的这个官网也几乎都是我自己写的。</p>
+    <p>所以如果强烈不建议你将此 UI 库用于生产环境。但如果你是抱着看源代码的目的来的那么这个库还是值得一看的</p>
+    <p>源代码放在了<a href="github.xxx">GitHub上</a></p>
+    <p>历史提交信息非常规范，你可以按提交的顺序逐个查看；你也可以直接查看每个组件的源代码和示例</p>
+    <p>运行方法见 README.md。</p>
+    <p>下一节：<a href="#/doc/install">安装</a></p>
+  </article>
 </template>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -6,10 +6,10 @@
         <h2>文档</h2>
         <ol>
           <li>
-            <router-link to="/doc/get-start">开始</router-link>
+            <router-link to="/doc/intro">介绍</router-link>
           </li>
           <li>
-            <router-link to="/doc/intro">介绍</router-link>
+            <router-link to="/doc/get-start">开始</router-link>
           </li>
           <li>
             <router-link to="/doc/install">安装</router-link>
